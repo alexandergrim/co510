9b1ba4d87282b332ef79d2de6024f4d52d805a9c
Dan Morrison <djdmorrison@gmail.com>
2014-02-19 23:22:11 +0000

    Added more to Find Record
    CSS for input[type=text] and buttons

--- application/views/view_findRecord.php
+++ application/views/view_findRecord.php
@@ -1,6 +1,23 @@
 <main>
 	
-	<h1>Find Record</h1><br /><br />
-	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	<h1>Find Record</h1>
+	<p>Type in the staff member's ID to find their records.</p><br />
 	
+	<article>
+	
+			<label for="textSearchId">Staff ID: </label>
+			<input type="text" id="textSearchId">
+	
+			<button class="submit"><i class="icon-search icon-white"></i>Find</button>
+	
+	</article>
+	
+	<article>
+		<label for="textFName">First Name</label>
+		<input type="text" id="textFName"><br />
+		
+		<label for="textSName">Surname</label>
+		<input type="text" id="textSName">
+	</article>
+
 </main>
